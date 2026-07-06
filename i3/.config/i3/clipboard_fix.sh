984e624421e9638047b13f84f478bad3f023618a
myname <PhantomInDaMachine@proton.me>
2026-07-06 19:00:06 -0400

    fixed greenclip issuse and now have it auto clear history on reload of i3 config

--- i3/.config/i3/clipboard_fix.sh
+++ i3/.config/i3/clipboard_fix.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-pkill vmtoolsd 2> /dev/null
-vmtoolsd -n vmusr
+#### For VirtualBox ####
+# pkill vmtoolsd 2> /dev/null
+# vmtoolsd -n vmusr
+#
+# VBoxClient --clipboard
 
-VBoxClient --clipboard
+
+#### For VMware ####
+# Kill the existing user-space tools daemon
+pkill vmtoolsd 2> /dev/null
+
+# Restart the user-space daemon explicitly
+# This handles clipboard, drag-and-drop, and resolution changes
+vmtoolsd -n vmusr &
